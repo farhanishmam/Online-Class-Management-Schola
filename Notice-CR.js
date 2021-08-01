b069d0a5d5095b9fbe0c25949e96211f70d81d49
Muntaha-adrita <55695122+Muntaha-adrita@users.noreply.github.com>
2021-08-01 16:23:02 +0600

    Create Notice-CR.js

---
+++ Notice-CR.js
@@ -0,0 +1,37 @@
+const closeButtons = document.getElementsByClassName("delete");
+for(i = 0; i<closeButtons.length; i++)
+{
+    closeButtons[i].addEventListener("click", function() 
+    {
+        this.parentElement.parentElement.style.display = 'none';
+        
+    })
+}
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("addNoticeBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
