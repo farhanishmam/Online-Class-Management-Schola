20865818ad8be5048d74c74a963c95b4a578d34e
ZettaiKurokaze <tasnim.ferdous5@gmail.com>
2021-08-03 11:45:05 +0600

    Routine SQL Completed

--- Routine.php
+++ Routine.php
@@ -1,3 +1,41 @@
+<?php
+	session_start();
+	include 'sql-conn.php';
+	$email = $_SESSION['Email'];
+	$result = $conn->query("SELECT COURSE_CODE, WEEKDAYS, SLOT, C_LINK FROM STUDENT_ROUTINE where ST_EMAIL = '$email' ");
+	$j=1;
+	function displayRoutine($j, $result) {
+		$row = $result->fetch_assoc();
+		for ( $i=1; $i<8; $i++){
+			if($row==NULL)		
+				break;				
+			elseif($i<4) {
+				if($j==$row['WEEKDAYS'] && $i==$row['SLOT'])
+					echo '<td style="text-align:center" class="column100 column'.($i+1).'" data-column="'.($i+1).'">'.$row['COURSE_CODE'].'</td>';
+				else {
+					echo '<td style="text-align:center" class="column100 column'.($i+1).'" data-column="'.($i+1).'"> </td>';
+					continue;
+				}
+			}
+			elseif($i==4){
+				echo '<td class="column100 column5" data-column="column5"> </td>';
+				continue;
+			}
+			else{
+				if($j==$row['WEEKDAYS'] && ($i-1)==$row['SLOT'])
+					echo '<td style="text-align:center" class="column100 column'.($i+1).'" data-column="'.($i+1).'">'.$row['COURSE_CODE'].'</td>';
+				else {
+					echo '<td style="text-align:center" class="column100 column'.($i+1).'" data-column="'.($i+1).'"> </td>';
+					continue;
+				}
+			}
+			if($i<7)
+				$row = $result->fetch_assoc();
+		}
+		return $result;
+	}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -37,11 +75,60 @@
 		<div class="container-table100">
 			<div class="wrap-table100">
 				<div id = "routine-table" class="table100 ver1 m-b-110">
-					<!-- Routine Table will be dynamically loaded -->
+				<table data-vertable="ver1">
+					<thead>
+						<tr class="row100 head">
+							<th class="column100 column1" data-column="column1"></th>
+							<th style="text-align:center" class="column100 column3" data-column="column2">08:30 - 09:45</th>
+							<th style="text-align:center" class="column100 column4" data-column="column3">10:00 - 11:15</th>
+							<th style="text-align:center" class="column100 column5" data-column="column4">11:30 - 12:45</th>
+							<th style="text-align:center" class="column100 column6" data-column="column5">12:45 - 02:15</th>
+							<th style="text-align:center" class="column100 column7" data-column="column6">02:15 - 03:30</th>
+							<th style="text-align:center" class="column100 column8" data-column="column7">03:45 - 05:00</th>
+							<th style="text-align:center" class="column100 column9" data-column="column8">05:15 - 06:30</th>
+						</tr>
+					</thead>
+						<?php
+						echo '<tbody>
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1">Monday</td>';
+								$result=displayRoutine($j, $result);
+								echo
+							'</tr>
+			
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1">Tuesday</td>';
+								$j++;
+								$result=displayRoutine($j, $result);
+								echo
+							'</tr>
+			
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1">Wednesday</td>';
+								$j++;
+								$result=displayRoutine($j, $result);
+								echo
+							'</tr>
+			
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1">Thursday</td>';
+								$j++;
+								$result=displayRoutine($j, $result);
+								echo
+							'</tr>
+			
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1">Friday</td>';
+								$j++;
+								$result=displayRoutine($j, $result);
+								echo
+							'</tr>
+						</tbody>';
+						?>
+					</table>
 				</div>
 			</div>
 		</div>
 	</div>
-	<script src = "Routine.js"></script>
 </body>
 </html>
